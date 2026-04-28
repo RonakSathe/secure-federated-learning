011461e5925d7f60adaca06948789305e4e511ec
Ronak Sathe <satheronak@gmail.com>
2026-04-28 12:25:49 +0530

    Changes in trainer

--- app/client/trainer.py
+++ app/client/trainer.py
@@ -1,11 +1,16 @@
+import random
 def label_data(data):
     cpu,memory,connections,bytes_sent,bytes_received = data
 
     score = 0
-    if cpu > 70: score += 1
-    if memory > 75: score += 1
-    if connections > 50: score += 1
-    if bytes_sent > 500000: score += 1
-    if bytes_received > 500000: score += 1
+    if cpu > 50: score += 1
+    if memory > 65: score += 1
+    if connections > 20: score += 1
+    if bytes_sent > 100000: score += 1
+    if bytes_received > 100000: score += 1
+
+    #FOrcing randomness
+    if random.random() < 0.3:
+        return 1
 
     return 1 if score >= 2 else 0
