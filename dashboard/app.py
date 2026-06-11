7e6fff99f2fd297fc14af449dbd59d4fbbbc5779
Ronak Sathe <satheronak@gmail.com>
2026-06-11 16:41:34 +0530

    Add Streamlit dashboard and metrics export for FL monitoring

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,6 +2,12 @@
 import streamlit as st
 from pathlib import Path
 import json
+import pandas as pd
+
+st.set_page_config(
+    page_title="Federated Learning Dashboard",
+    layout="wide"
+)
 
 BASE_DIR = Path(__file__).parent
 
@@ -10,27 +16,64 @@ metrics_file = BASE_DIR/"metrics.json"
 st.title("Federated Learning Dashboard")
 
 #opening file: 
-with open(metrics_file,"r") as f:
-    
+with open(metrics_file,"r") as f:    
     #data variable holding all the contents of the metrics.json file as json
     data = json.load(f)
+train_df = pd.DataFrame.from_dict(data["train_metrics"],orient="index")
+eval_df = pd.DataFrame.from_dict(data["evaluate_metrics"],orient="index")
+train_df.index.name = "Round"
+eval_df.index.name = "Round"
 
-    st.metric(
-        "Current Round",
-        data["round"]
-    )
+st.header("Training Summary")
+col1,col2,col3,col4 = st.columns(4)
 
-    st.metric(
-        "Accuracy",
-        data["accuracy"]
-    )
+latest_train_acc = train_df["train_accuracy"].iloc[-1]
+latest_train_loss = train_df["train_loss"].iloc[-1]
 
-    st.metric(
-        "Loss",
-        data["loss"]
-    )
+latest_eval_acc = eval_df["accuracy"].iloc[-1]
+latest_eval_loss = eval_df["loss"].iloc[-1]
 
-    st.metric(
-        "Clients",
-        data["clients"]
-    )
+col1.metric(
+    "Train Accuracy",
+    f"{latest_train_acc:.4f}"
+)
+
+col2.metric(
+    "Train loss",
+    f"{latest_eval_loss:.4f}"
+)
+
+col3.metric(
+    "Eval Accuracy",
+    f"{latest_eval_acc:.4f}"
+)
+
+col4.metric(
+    "Eval loss",
+    f"{latest_eval_loss:.4f}"
+)
+
+#Accuracy Chart
+st.header("Accuracy Over Rounds")
+accuracy_df = pd.DataFrame({
+    "Train Accuracy": train_df["train_accuracy"],
+    "Eval Accuracy": eval_df["accuracy"]
+})
+
+st.line_chart(accuracy_df)
+
+#Loss Chart
+st.header("Loss Over Rounds")
+loss_df = pd.DataFrame({
+    "Train Loss": train_df["train_loss"],
+    "Eval Loss": eval_df["loss"]
+})
+
+st.line_chart(loss_df)
+
+
+#Raw Metrics
+st.header("Training Metrics")
+st.dataframe(train_df)
+st.header("Evaluation Metrics")
+st.dataframe(eval_df)
